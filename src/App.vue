2448c06a3d6ecf162d8c685e3a241a6d21cb3a30
wahyuMar <wahyumarrpl@gmail.com>
2026-09-23 09:16:53 +0700

    feat: add initial project setup with Vue 3, Vite, and Tailwind CSS
    - Created main application structure with App.vue and main.ts
    - Added HelloWorld component with basic counter functionality and logos
    - Implemented ProductsForm component for product input
    - Introduced SVG icons for social media and documentation
    - Added assets including hero image and logos for Vite and Vue
    - Configured Tailwind CSS for styling
    - Set up TypeScript configuration files for app and node
    - Created vite.config.ts for Vite setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+import ProductsForm from './components/products/ProductsForm.vue'
+</script>
+
+<template>
+  <ProductsForm/>
+</template>
---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    tailwindcss
+  ],
+})
